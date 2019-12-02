d0033d4423347af5c3c7ac46c6e787b84a02d968
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2019-12-02 10:33:01 +0100

    02.2019

---
+++ 2019/02.ts
@@ -0,0 +1,46 @@
+import { getInput } from './utils.ts';
+const enum OptCode {
+    ADD = 1,
+    MULTIPLY = 2,
+    STOP = 99,
+}
+
+const WALK = 4;
+
+const raw = await getInput();
+
+const d = raw.split(',').map(Number);
+
+function run(data: number[]) {
+    for (let i = 0; i < data.length; i = i + WALK) {
+        const optcode: OptCode = data[i];
+        const posI1 = data[i + 1];
+        const posI2 = data[i + 2];
+        const posO = data[i + 3];
+
+        switch(optcode) {
+            case OptCode.STOP:
+                return data[0];
+            case OptCode.ADD:
+                data[posO] = data[posI1] + data[posI2];
+                break;
+            case OptCode.MULTIPLY:
+                data[posO] = data[posI1] * data[posI2];
+                break;
+            default: throw new Error("1202Exception ! Wrong optcode !")
+        }
+    }
+    return data[0];
+}
+
+for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 0; verb <= 99; verb++) {
+        let data = [...d];
+        data[1] = noun;
+        data[2] = verb;
+        if (run(data) === 19690720) {
+            console.log(100 * noun + verb);
+            Deno.exit();
+        }
+    }
+}
